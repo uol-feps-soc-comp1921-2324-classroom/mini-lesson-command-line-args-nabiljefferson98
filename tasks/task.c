b4c0b96ed460cfcc9820348697710a2c6c1ea40e
Muhammad Nabil Bin Muhammad Saiful Wong <146673341+nabiljefferson98@users.noreply.github.com>
2024-02-19 13:06:27 +0000

    finished week 1 task

--- tasks/task.c
+++ tasks/task.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Add 2 number after ./task1 when execute because it'll save as array
+// ex: ./task1 <num1> <num2>
+
 int main(int argc, char *argv[]) {
     
+    // appropiate validation
     if (argc != 3){
 
         printf("Usage: %s <num1> <num2>\n", argv[0]);
         return 0;
     }
     
+    // function sum
     int num1, num2, sum;
+    // convert string into int because argv[] is always string since its an array
     num1 = atoi(argv[1]);
     num2 = atoi(argv[2]);
     sum = num1 + num2;
+    // printf the result
     printf("Sum of %s and %s is = %d\n", argv[1], argv[2], sum);
     return 0; // Exit successfully
 
